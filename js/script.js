7b0460b2a770ce9699113095f16fc9b0a99205ba
Сергій Ольшанський <seven8959@gmail.com>
2026-04-29 14:17:35 +0300

    fix: scroll to form

--- js/script.js
+++ js/script.js
@@ -50,24 +50,34 @@ window.addEventListener("scroll", animateProgressBars);
 
 // scroll
 var linkNav = document.querySelectorAll('[href^="#"]'),
-V = 0.2;
+    V = 0.2;
 for (var i = 0; i < linkNav.length; i++) {
-    linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
-        e.preventDefault(); //отменяем стандартное поведение
-        var w = window.pageYOffset,  // производим прокрутка прокрутка
-        hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
-        t = document.querySelector(hash).getBoundingClientRect().top - 120,  // отступ от окна браузера до id
-        start = null;
-        requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+    linkNav[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        var w = window.pageYOffset,
+            hash = this.href.replace(/[^#]*(.*)/, '$1');
+        var screenWidth = window.innerWidth;
+        var offset = 120;
+        if (screenWidth >= 320 && screenWidth < 400) {
+            offset = +120;
+        } else if (screenWidth >= 400 && screenWidth < 768) {
+            offset = 120;
+        }
+        var t = document.querySelector(hash).getBoundingClientRect().top - offset,
+            start = null;
+        requestAnimationFrame(step);
         function step(time) {
             if (start === null) start = time;
             var progress = time - start,
-            r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
-            window.scrollTo(0,r);
+                r = (t < 0
+                    ? Math.max(w - progress / V, w + t)
+                    : Math.min(w + progress / V, w + t)
+                );
+            window.scrollTo(0, r);
             if (r != w + t) {
-                requestAnimationFrame(step)
+                requestAnimationFrame(step);
             } else {
-                location.hash = hash  // URL с хэшем
+                location.hash = hash;
             }
         }
     }, false);
